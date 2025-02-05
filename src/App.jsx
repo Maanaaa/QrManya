c6c12e84cf52a7d2c6617ffd6de057ac0d622b8e
Théo <manya.th@icloud.com>
2025-02-05 18:50:39 +0100

    refractor: Clean up unused libraries and imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from '../src/components/layout/Header.jsx'
 import Form from '../src/components/ui/Form.jsx'
-import DisplayQrCode from './components/qrCode/QrCodeDisplay.jsx'
-import QRCode from "react-qr-code"
 
 import './App.css'
 
